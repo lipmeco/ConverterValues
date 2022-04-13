6bc9a35edc4118e2fe20493421e81b23e8dbc203
Maxim Kuznetsov <maksismart200@gmail.com>
2022-04-13 14:12:57 +0300

    83290

--- converter/converter/Model/Currency.swift
+++ converter/converter/Model/Currency.swift
@@ -1,14 +1,7 @@
-//
-//  Currency.swift
-//  converter
-//
-//  Created by Максим Кузнецов on 27.03.2022.
-//
-
 import Foundation
 
 struct Currency {
-    let name: String
+    var name: String
     let cost: Double
 }
 
